e8734e5c11746b30a072eefc9564337cb4f365f2
ГРИГОРИЙ <zemlyanscky.grigorij@yandex.ru>
2017-12-02 12:39:59 +0400

    доделано 01.12.2017

--- src/main/java/com/javasampleapproach/log4j2/aspect/LoggingAdvisor.java
+++ src/main/java/com/javasampleapproach/log4j2/aspect/LoggingAdvisor.java
@@ -1,11 +1,14 @@
 package com.javasampleapproach.log4j2.aspect;
 
 import com.javasampleapproach.log4j2.logging.Auditing;
+import com.javasampleapproach.log4j2.logging.LoggerBaseAuditor;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import sun.util.logging.resources.logging;
@@ -22,12 +25,18 @@ public class LoggingAdvisor {
 
     private final Auditing auditing;
 
-   /// @Before("com.javasampleapproach.log4j2.controller.aspect.LoggingPointcut.logService()")
+
     @Before("com.javasampleapproach.log4j2.aspect.LoggingPointcut.logService()")
-//@Pointcut("execution(public * com.javasampleapproach.log4j2.controller.service..*(..))")
+//@Pointcut("execution(public * com.javasampleapproach.log4j2.service..*(..))")
     public void serviceLogBeforeAdvice(final JoinPoint joinPoint) {
         auditing.logService(joinPoint.getSignature().getDeclaringTypeName(),
                 joinPoint.getSignature().getName(),
                 Arrays.toString(joinPoint.getArgs()));
+
+       /* LoggerBaseAuditor logAudi = new LoggerBaseAuditor();
+        logAudi.logService(joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs()));*/
+
     }
 }
--- src/main/java/com/javasampleapproach/log4j2/controller/SimpleController.java
+++ src/main/java/com/javasampleapproach/log4j2/controller/SimpleController.java
@@ -1,20 +1,29 @@
 package com.javasampleapproach.log4j2.controller;
 
 import com.javasampleapproach.log4j2.domain.NewClass;
+import com.javasampleapproach.log4j2.service.NewClassService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestController
-public class SimpleController  implements ErrorController{
+public class SimpleController  {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static final String PATH = "/error";
+    @Autowired
+    private NewClassService newClassService;
+
     @RequestMapping("/")
     String hello(){
         NewClass newClass = new NewClass();
@@ -44,18 +53,20 @@ public class SimpleController  implements ErrorController{
     }
 
 
-    @RequestMapping(value = PATH)
-    public String error() {
+//    @RequestMapping(value = PATH)
+//    public String error() {
+//
+//
+//
+//        return  "Страница не найдена";
+//    }
 
-        /*PageNotFoundException ex =new PageNotFoundException();
-        throw   ex;*/
-        return  "Страница не найдена";
+
+
+/*
+    @GetMapping(value = "/getList")
+    public ResponseEntity<List<String>> getStringList(){
+        return new ResponseEntity<List<String>>(newClassService.getList(), HttpStatus.OK);
     }
-
-    @Override
-    public String getErrorPath() {
-        return "asdfasd";
-    }
-
-
+*/
 }
